32ffe188450d7f3c16f359e0dad57b7cb173df49
veeereshnaik7 <jerryveeru1@gmail.com>
2024-01-02 06:38:54 +0530

    task finished

---
+++ 29-dec/index.js
@@ -0,0 +1,19 @@
+//  task-1 pattern
+var a = "******";
+var b = "";
+for (i = 0; i <= a.length - 1; i++) {
+  b = b + a[i];
+  console.log(b);
+}
+console.log("\n", "\n", "\n");
+
+//  task-2 pattern
+var a = "******";
+var b = "";
+for (i = 0; i <= a.length - 1; i++) {
+  for (j = 0; j <= a.length - 1 - i; j++) {
+    b += a[j];
+  }
+  b += "\n";
+}
+console.log(b);
