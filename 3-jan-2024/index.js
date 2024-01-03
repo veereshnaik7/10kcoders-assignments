0341914ab583d823a5e7accc4ff6f6527fbd65f0
veeereshnaik7 <jerryveeru1@gmail.com>
2024-01-03 10:03:47 +0530

    changes

---
+++ 3-jan-2024/index.js
@@ -0,0 +1,36 @@
+
+        
+        ///   task -1
+
+        var a="veeresh naik ramavath";
+        console.log(a);
+        console.log("number of characters" ,a.length)
+        var b=a.split(" ")
+        console.log(b, "no of words are ",b.length)
+        
+        
+        console.log("\n")
+        
+        
+        ///   task -2
+        var a="veereshnaik";
+        console.log(a )
+        var b=a.split("")
+        console.log(b)
+        console.log(String(Array(b.join("-"))))
+        
+        
+        
+        console.log("\n")
+        
+        /// task-3
+        var a=[7,7,0,2,5,1,3,3,7,6];
+        console.log(a);
+        for (var i=0; i<=a.length-1; i++){
+            var b=(Number(a[i]))+1
+         
+            console.log(b)
+        }
+
+        
+    
